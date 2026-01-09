056fb61b0247d85fcbfb7dc88e91234915cfebef
Dilan <dilanavishka183@gmail.com>
2026-01-09 13:25:25 +0530

    👌

--- frontend-Intership-Assignment/src/App.jsx
+++ frontend-Intership-Assignment/src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
       <h1 className="text-4xl font-bold text-center mb-6 text-blue-600">
-        Product List
+        Product List_
       </h1>
       <ProductList />
     </div>
